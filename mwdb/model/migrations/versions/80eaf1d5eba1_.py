57c23d1f4d731eadd1865fb05f987de2d5efeff6
Sharon Yankovich <49753115+yankovs@users.noreply.github.com>
2026-01-24 07:35:00 +0000

    test

---
+++ mwdb/model/migrations/versions/80eaf1d5eba1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 80eaf1d5eba1
+Revises: 71fd616e8096, ab12cd34ef56
+Create Date: 2026-01-24 07:34:40.632579
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '80eaf1d5eba1'
+down_revision = ('71fd616e8096', 'ab12cd34ef56')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
